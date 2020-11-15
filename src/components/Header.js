7eee36e63d5e66404291ce99d5b5e2f722a91cef
mdahmedmaruf <mdahmedmaruf@gmail.com>
2020-11-15 19:05:36 +0600

    header done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,34 @@
 import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import logo from "../images/Logo.png";
 
 const Header = () => {
   return (
     <header>
-      <h2>header</h2>
+      <Navbar bg="light" variant="light" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="#home">
+            <img src={logo} alt="" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">About</Nav.Link>
+              <Nav.Link href="#link">Services</Nav.Link>
+              <Nav.Link href="#link">Concerns</Nav.Link>
+              <Nav.Link href="#link">Event</Nav.Link>
+              <Nav.Link href="#link">Contact</Nav.Link>
+              <Nav.Link
+                href="#link"
+                className="login-button bg-primary text-white rounded-sm"
+              >
+                Login
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </header>
   );
 };
