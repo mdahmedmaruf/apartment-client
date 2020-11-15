0883d604213416b91a8f7ad0fbc369c7d8392e4c
mdahmedmaruf <mdahmedmaruf@gmail.com>
2020-11-15 22:38:53 +0600

    add hero section

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import { Container } from "react-bootstrap";
-import "./App.css";
+import Banner from "./components/Banner";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Home from "./pages/Home";
+import "./sass/App.scss";
 
 function App() {
   return (
     <>
       <Header />
       <main>
+        <Banner />
         <Container>
           <h1>Welcome</h1>
+          <Home />
         </Container>
       </main>
       <Footer />
